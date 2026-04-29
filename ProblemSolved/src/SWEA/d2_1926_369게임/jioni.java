958fcb6ec9a4aaf157fb5e0ab4ca3e4f43cca755
Jioni <rnrmfwldwlddl@gmail.com>
2026-04-29 21:14:00 +0900

    SWEA 1926

---
+++ ProblemSolved/src/SWEA/d2_1926_369게임/jioni.java
@@ -0,0 +1,9 @@
+package SWEA.d2_1926_369게임;
+import java.util.*;
+
+public class jioni {
+    public static void main(String []args){
+//        Scanner sc =new Scanner(System.in);
+//        int N =sc.nextInt();
+    }
+}
